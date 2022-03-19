cb10b6775a0a3b65be2b213d593e99657e64e47a
Eduard Aksamitov <e@edaks.co>
2022-03-19 19:41:51 +0300

    feat: move to new domain

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,17 @@ header.h
 	nav
 		ul.h__ul
 			li
-				a.h__l(href="https://instagram.com/suprematistco" target="_blank" rel="noopener") Instagram
+				a.h__l(href="https://instagram.com/suprematistcc" target="_blank" rel="noopener") Instagram
 			li
-				a.h__l(href="https://facebook.com/suprematistco" target="_blank" rel="noopener") Facebook
+				a.h__l(href="https://facebook.com/suprematistcc" target="_blank" rel="noopener") Facebook
 			li
-				a.h__l(href="https://twitter.com/suprematistco" target="_blank" rel="noopener") Twitter
+				a.h__l(href="https://twitter.com/suprematistcc" target="_blank" rel="noopener") Twitter
 			li
-				a.h__l(href="https://t.me/suprematistco" target="_blank" rel="noopener") Telegram
+				a.h__l(href="https://t.me/suprematistcc" target="_blank" rel="noopener") Telegram
 			li
-				a.h__l(href="https://pinterest.com/suprematistco" target="_blank" rel="noopener") Pinterest
+				a.h__l(href="https://pinterest.com/suprematistcc" target="_blank" rel="noopener") Pinterest
 			li
-				a.h__l(href="https://vk.com/suprematistco" target="_blank" rel="noopener") VK
+				a.h__l(href="https://vk.com/suprematistcc" target="_blank" rel="noopener") VK
 
 </template>
 
